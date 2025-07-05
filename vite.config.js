24185011f6bb230b7550f0d54d55883e3b4aab6d
Haeqal <salehudinhaeqal@gmail.com>
2025-07-05 16:01:11 +0700

    Fix vite config js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    // Menambahkan konfigurasi build untuk mencegah penghapusan direktori output
+    build: {
+        emptyOutDir: false,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
